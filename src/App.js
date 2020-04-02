a4167900e39bb265f61f9a7365252ecba57be1da
John Fairclough <johnmfairclough@gmail.com>
2020-04-02 12:00:24 +0100

    files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,102 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Axios from "axios";
+import "./style.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+constructor(props) {
+  super(props);
+
+  this.getCountryData = this.getCountryData.bind(this);
 }
 
-export default App;
+  state = {
+    confirmed: 0,
+    recovered: 0,
+    deaths: 0,
+    countries: []
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  async getData() {
+    const resApi = await Axios.get("https://covid19.mathdro.id/api");
+    const resCountries = await Axios.get("https://covid19.mathdro.id/api/countries");
+    const countries = [];
+       for(var i = 0; i < resCountries.data.countries.length; i++){
+           countries.push(resCountries.data.countries[i].name);
+       }
+
+
+    this.setState({
+      confirmed: resApi.data.confirmed.value,
+      recovered: resApi.data.recovered.value,
+      deaths: resApi.data.deaths.value,
+      countries
+    });
+  }
+
+  async getCountryData(e) {
+    if (e.target.value === "Worldwide") {
+      return this.getData();
+    }
+    try{
+    const res = await Axios.get(`https://covid19.mathdro.id/api/countries/${e.target.value}`);
+      this.setState({
+        confirmed: res.data.confirmed.value,
+        recovered: res.data.recovered.value,
+        deaths: res.data.deaths.value,
+      });
+    }
+    catch (err) {
+      if (err.response.status === 404)
+      this.setState({
+        confirmed: "No data available",
+        recovered: "No data available",
+        deaths: "No data available"
+      });
+    }
+  }
+
+  renderCountryOptions() {
+    return this.state.countries.map((country, i) => {
+      return <option key={i}>{country}</option>
+    });
+  }
+
+  render () {
+    return (
+      <div className="app">
+      <div className="container">
+        <h1>COVID-19 Update</h1>
+
+        <select onChange={this.getCountryData}>
+        <option>Worldwide</option>
+          {this.renderCountryOptions()}
+        </select>
+
+        <div className="flex">
+          <div className="box confirmed">
+            <h3>Confirmed</h3>
+            <h4>{this.state.confirmed}</h4>
+          </div>
+          <div className="box recovered">
+            <h3>Recovered</h3>
+            <h4>{this.state.recovered}</h4>
+          </div>
+          <div className="box deaths">
+            <h3>Deaths</h3>
+            <h4>{this.state.deaths}</h4>
+          </div>
+          </div>
+          </div>
+          <div>
+          <a class="mailto" href="mailto:Johnmfairclough@gmail.com?subject=Hello%20John">Johnmfairclough@gmail.com</a>
+          </div>
+
+        </div>);
+
+
+  }
+}
