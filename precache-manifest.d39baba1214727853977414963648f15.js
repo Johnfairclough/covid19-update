ee6527387921554ffbb0ab41445ee1fdf0355c02
John Fairclough <johnmfairclough@gmail.com>
2020-04-02 12:06:13 +0100

    Updates

---
+++ precache-manifest.d39baba1214727853977414963648f15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff5e65e5434af76b7a9806c8b1d613f4",
+    "url": "/covid19-update/index.html"
+  },
+  {
+    "revision": "783c0747cbdbfb3a9bb3",
+    "url": "/covid19-update/static/css/main.d297a99b.chunk.css"
+  },
+  {
+    "revision": "a09a94347e6e703983cd",
+    "url": "/covid19-update/static/js/2.c3b618a2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid19-update/static/js/2.c3b618a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "783c0747cbdbfb3a9bb3",
+    "url": "/covid19-update/static/js/main.fb3c1eb8.chunk.js"
+  },
+  {
+    "revision": "5c44ca62483ea88baa31",
+    "url": "/covid19-update/static/js/runtime-main.3380e454.js"
+  }
+]);
